0346727893a2597d926e0ca8172a9cb8eb3e582f
Dewitt Buckingham <dj729@comcast.net>
2015-03-18 01:36:27 -0700

    Building it up
    Kind of struggling my way through this. Think I should probably put some
    more thought into the design.

--- toc.js
+++ toc.js
@@ -1,13 +1,144 @@
 var Toc = (function() {
 
-    // Variables
-    var Toc = {};
+    /**
+     * Variables
+     */
+    var Toc = {},
+        headings = 'h1, h2, h3, h4, h5, h6';
 
-    // Map out Headings
-    var mapHeadings = function( element ) {
+    /**
+     * Methods
+     */
+    
+    /**
+     * Borrowed forEach() that works across Arrays, Objects and NodeLists
+     * @author Chris Ferdinandi, http://github.com/cferdinandi/smooth-scroll
+     * @license http://gomakethings.com/mit/ MIT License
+     *
+     * 
+     * @private
+     * @param {Array|Object|NodeList} collection Collection of items to iterate over
+     * @param {Function} callback Callback function for each iteration
+     * @param {Array|Object|NodeList} scope Object/NodeList/Array that forEach is iterating over (aka `this`)
+     */
+     var forEach = function (collection, callback, scope) {
+        if (Object.prototype.toString.call(collection) === '[object Object]') {
+            for ( var prop in collection ) {
+                if(Object.prototype.hasOwnProperty.call(collection, prop)) {
+                    callback.call(scope, collection[prop], prop, collection);
+                }
+            }
+        } else {
+            for (var i = 0, len = collection.length; i< len; i++) {
+                callback.call(scope, collection[i], i, collection);
+            }
+        }
+     };
 
+    /**
+    * Generates a simple random string to represent an unique ID
+    *
+    * @private
+    */
+    var createId = function () {
+        return '_' + Math.random().toString(36).substr(2, 9);
+    };
+    
+    /**
+     * Checks all Heading Tags (h1-h6) for a unique ID.
+     * If one doesn't exist, it creates one based.
+     *
+     *
+     * @private
+     * @param {NodeList} nodeList NodeList of the elements being checked for IDs
+     */
+    var identify = function( nodeList ) {
+        forEach(nodeList, function( element ) {
+            element.id = element.id || createId();
+        });
+    };
+
+    /**
+     * Builds a map of all headings
+     * @param {NodeList} nodeList NodeList of Heading Elements
+     * @return {Array} map Map of the headings vital details
+     */
+    var mapHeadings = function( nodeList ) {
+        var map = [].map.call(nodeList, function( element, i ) {
+            var level = Number.parseInt(element.tagName.slice(1));
+            var obj = {
+                'id'        : element.id,
+                'text:'     : element.textContent,
+                'children'  : null
+            };
+
+            if (element === null) {
+                return;
+            }
+            
+            return obj;
+        });
+
+        return map;
+    };
+
+    // Build Navigation Element
+    var build = function( map ) {
+        var level, child;
+        var nav = document.querySelector('nav');
+
+        forEach(map, function(element) {
+            if (level === undefined) {
+                level = element.level;
+                var ul = document.createElement('ul');
+                ul.appendChild(listItem(element));
+                nav.appendChild(ul);
+            } else if (level < element.level) {
+                level = element.level;
+                child = ulItem(element);
+            } else if (level === element.level) {
+                if(child !== undefined) {
+                    child.appendChild(listItem(element));
+                } else {
+                    nav.lastChild.appendChild(listItem(element));
+                }
+            } else if (level > element.level) {
+                child = undefined;
+                level = element.level;
+            }
+        });
+    };
+
+    var ulItem = function (obj) {
+        var ul = document.createElement('ul');
+        ul.appendChild(listItem(obj));
+
+        return ul;
+    };
+
+    var listItem = function( obj ) {
+        var a = document.createElement('a');
+        var li = document.createElement('li');
+
+        a.href = '#' + obj.id;
+        a.textContent = obj.text;
+
+        li.appendChild(a);
+
+        return li;
+    };
+
+    Toc.run = function() {
+        var nodeList = document.querySelectorAll(headings);
+
+        // Run
+        identify(nodeList);
+        var map = mapHeadings(nodeList);
+        console.log(map);
+
+        build(map);
     };
 
     // Return Public API
     return Toc;
-});
+})();
